409a2c3c10e9b4ee20d54f490c90c1dc26858526
Zehua Lu <lucas.zeh.lu@gmail.com>
2025-08-13 23:28:51 +0200

    Refactor pretokenize_and_count by unnecessary loop

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -172,26 +172,64 @@ class Tokenizer:
             
     #     return token_count
 
+    # @staticmethod
+    # def pretokenize_and_count(docs: list[str], gpt2_regex: bool = False) -> dict[tuple[bytes], int]:
+    #     token_count : dict[tuple[bytes], int] = {}
+    #     token_count_get = token_count.get
+    
+    #     for doc in docs:
+    #         pre_tokens = None
+    #         # Use a regex-based pre-tokenizer (used by GPT-2; Radford et al., 2019)
+    #         if gpt2_regex:
+    #             # PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+    #             pre_tokens = PAT.finditer(doc)
+    #             pre_tokens = [match.group(0) for match in pre_tokens]
+    #         else:
+    #             pre_tokens = doc.split()
+    
+    #         for token in pre_tokens:
+    #             bytes_token = token.encode("utf-8")
+                
+    #             tuple_bytes_token = tuple(bytes_token[i : i+1] for i in range (len(bytes_token)))
+    #             token_count[tuple_bytes_token] = token_count_get(tuple_bytes_token, 0) + 1
+            
+    #     return token_count
+    
+    # #OPED
     @staticmethod
     def pretokenize_and_count(docs: list[str], gpt2_regex: bool = False) -> dict[tuple[bytes], int]:
         token_count : dict[tuple[bytes], int] = {}
         token_count_get = token_count.get
     
         for doc in docs:
-            pre_tokens = None
+            # pre_tokens = None
             # Use a regex-based pre-tokenizer (used by GPT-2; Radford et al., 2019)
             if gpt2_regex:
-                # PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-                pre_tokens = PAT.finditer(doc)
-                pre_tokens = [match.group(0) for match in pre_tokens]
+                for token in PAT.finditer(doc):
+                    token_str = token.group(0)
+                    bytes_token = token_str.encode("utf-8")
+
+                    tuple_bytes_token = tuple(bytes_token[i : i+1] for i in range (len(bytes_token)))
+                    token_count[tuple_bytes_token] = token_count_get(tuple_bytes_token, 0) + 1
             else:
-                pre_tokens = doc.split()
+                for token in doc.split():
+                    bytes_token = token.encode("utf-8")
+
+                    tuple_bytes_token = tuple(bytes_token[i : i+1] for i in range (len(bytes_token)))
+                    token_count[tuple_bytes_token] = token_count_get(tuple_bytes_token, 0) + 1
+
+            # if gpt2_regex:
+            #     # PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+            #     pre_tokens = PAT.finditer(doc)
+            #     pre_tokens = [match.group(0) for match in pre_tokens]
+            # else:
+            #     pre_tokens = doc.split()
     
-            for token in pre_tokens:
-                bytes_token = token.encode("utf-8")
+            # for token in pre_tokens:
+            #     bytes_token = token.encode("utf-8")
                 
-                tuple_bytes_token = tuple(bytes_token[i : i+1] for i in range (len(bytes_token)))
-                token_count[tuple_bytes_token] = token_count_get(tuple_bytes_token, 0) + 1
+            #     tuple_bytes_token = tuple(bytes_token[i : i+1] for i in range (len(bytes_token)))
+            #     token_count[tuple_bytes_token] = token_count_get(tuple_bytes_token, 0) + 1
             
         return token_count
     
