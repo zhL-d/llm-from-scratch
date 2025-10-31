f2b31a7013e87691d6b3aa5faea37cf8993619a8
Zehua Lu <lucas.zeh.lu@gmail.com>
2025-10-31 21:00:29 +0100

    feat: add RMSNorm(einx version) implementation and update import in adapters.py

--- tests/adapters.py
+++ tests/adapters.py
@@ -13,7 +13,7 @@ from cs336_basics.bpetokenizer_trainer import BPETokenizerTrainer
 from cs336_basics.tokenizer import Tokenizer
 from cs336_basics.linear_module import Linear
 from cs336_basics.embedding import Embedding
-from cs336_basics.rmsnorm import RMSNorm
+from cs336_basics.rmsnorm_einx import RMSNorm
 
 
 
@@ -390,9 +390,11 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
+
     rms_layer = RMSNorm(d_model, eps)
     rms_layer.load_state_dict({"g": weights})
     return rms_layer.forward(in_features)
+   
     # raise NotImplementedError
 
 
